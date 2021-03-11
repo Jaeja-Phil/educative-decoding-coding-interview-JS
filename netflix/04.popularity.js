e9d49e87fa6b6040c6b00e5b473bfa26b87eebfd
Phillip Choi <phillip@Phillips-Mac-mini.local>
2021-03-11 15:17:33 +0900

    add description

---
+++ netflix/04.popularity.js
@@ -0,0 +1,14 @@
+/**
+ * Netflix has popularity score.
+ *
+ * That's how they keep track of what movie users liked and disliked.
+ * This popularity score is based on likes/dislikes and feedbacks and is
+ * updated weekly. Feedbacks are "pushed" at the end of the list.
+ *
+ * Base on the list, find out if the movie is increasing or decreasing in popularity
+ * If the rating is fluctuating, let it be known so
+ */
+
+/**
+ * Implementations
+ */
